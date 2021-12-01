c7c11f189b6a57d5a61fdcb0a241ac6deb6a69bd
Shi <shitiencheung@gmail.com>
2021-12-01 15:17:20 +0100

    Alle filer

---
+++ main.js
@@ -0,0 +1,11 @@
+/* Javascript taget fra Youtube https://www.youtube.com/watch?v=oYRda7UtuhA&t=1337s&ab_channel=EasyTutorials */
+
+var navLinks = document.getElementById("navLinks");
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
